c1b102bfa3d4b517063617b2f0ed42d642bf3ecc
Bảo An <66411880+dangthibaoan@users.noreply.github.com>
2021-08-29 16:18:52 +0700

    Initial commit

---
+++ app/src/main/java/com/example/happyfarm/HappyFarmScreen.java
@@ -0,0 +1,14 @@
+package com.example.happyfarm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HappyFarmScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_happy_farm_screen);
+    }
+}
