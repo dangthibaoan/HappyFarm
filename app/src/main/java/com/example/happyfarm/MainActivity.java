76707ada08641d9fa6c71d4b4b976ceaaf2b269a
Bảo An <66411880+dangthibaoan@users.noreply.github.com>
2021-08-31 23:37:33 +0700

    update giao dien game

--- app/src/main/java/com/example/happyfarm/MainActivity.java
+++ app/src/main/java/com/example/happyfarm/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.happyfarm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+
+        Thread wait = new Thread(){
+            public void run(){
+                try {
+                    sleep(5000);
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_LONG).show();
+                } finally {
+                    startActivity(new Intent(MainActivity.this,HappyFarmScreen.class));
+                }
+            }
+        };
+        wait.start();
     }
 }
--- app/src/main/java/com/example/happyfarm/MainActivity2.java
+++ app/src/main/java/com/example/happyfarm/MainActivity2.java
@@ -3,12 +3,21 @@ package com.example.happyfarm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.Objects;
 
 public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main2);
     }
 }
