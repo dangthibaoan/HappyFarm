dfc7d3d5d6b3a48bdbcddf37189148dbbcc66932
Bảo An <66411880+dangthibaoan@users.noreply.github.com>
2021-08-31 15:31:40 +0700

    update model

--- app/src/main/java/com/example/happyfarm/Model/Plants.java
+++ app/src/main/java/com/example/happyfarm/Model/Plants.java
@@ -5,22 +5,22 @@ import java.util.Date;
 public class Plants {
     private String uid;         //uid của player
     private int plant_id;       //0 - lúa, 1 - cà chua, 2 - cà rốt
+    private int plant_lvl;      //1 - cấp 1, 2 - cấp 2,... :cấp bậc giống cây
+    private int plant_sl;       //sản lượng cây trồng cơ bản = cấp bậcx10, vd: lúa cấp 1 sản lượng 1x10=10
     private int plant_status;   //0 - chưa trồng, 1 - đã trồng chưa chín, 2 - chín
-    private boolean watering;   //false - chưa tưới, true - đã tưới
-    private boolean bon_phan;   //false - chưa bón, true - đã bón
+    private int land_status;    //0 - chưa làm đất, 1 -  đã làm đất, 2 - đã tưới, 3 - đã bón phân, 4 - đã tưới+bón phân
     private Date time_1;        //thời gian trồng
-    private Date time_2;        //thời gian thu hoạch
 
     public Plants() { }
 
-    public Plants(String uid, int plant_id, int plant_status, boolean watering, boolean bon_phan, Date time_1, Date time_2){
+    public Plants(String uid, int plant_id, int plant_lvl, int plant_sl, int plant_status, int land_status, Date time_1){
         this.uid = uid;
         this.plant_id = plant_id;
+        this.plant_lvl = plant_lvl;
+        this.plant_sl = plant_sl;
         this.plant_status = plant_status;
-        this.watering = watering;
-        this.bon_phan = bon_phan;
+        this.land_status = land_status;
         this.time_1 = time_1;
-        this.time_2 = time_2;
     }
 
     public String getUid() {
@@ -39,6 +39,22 @@ public class Plants {
         this.plant_id = plant_id;
     }
 
+    public int getPlant_lvl() {
+        return plant_lvl;
+    }
+
+    public void setPlant_lvl(int plant_lvl) {
+        this.plant_lvl = plant_lvl;
+    }
+
+    public int getPlant_sl() {
+        return plant_sl;
+    }
+
+    public void setPlant_sl(int plant_sl) {
+        this.plant_sl = plant_sl;
+    }
+
     public int getPlant_status() {
         return plant_status;
     }
@@ -47,20 +63,12 @@ public class Plants {
         this.plant_status = plant_status;
     }
 
-    public boolean isWatering() {
-        return watering;
+    public int getLand_status() {
+        return land_status;
     }
 
-    public void setWatering(boolean watering) {
-        this.watering = watering;
-    }
-
-    public boolean isBon_phan() {
-        return bon_phan;
-    }
-
-    public void setBon_phan(boolean bon_phan) {
-        this.bon_phan = bon_phan;
+    public void setLand_status(int land_status) {
+        this.land_status = land_status;
     }
 
     public Date getTime_1() {
@@ -69,13 +77,5 @@ public class Plants {
 
     public void setTime_1(Date time_1) {
         this.time_1 = time_1;
-    }
-
-    public Date getTime_2() {
-        return time_2;
-    }
-
-    public void setTime_2(Date time_2) {
-        this.time_2 = time_2;
     }
 }
